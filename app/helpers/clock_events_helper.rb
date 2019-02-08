94fee651c044ddcd1c2f775f87a550ea164aa020
Annie Chen <anniebh30@gmail.com>
2019-02-07 21:04:34 -0500

    fix time.zone display

--- app/helpers/clock_events_helper.rb
+++ app/helpers/clock_events_helper.rb
@@ -1,16 +1,16 @@
 module ClockEventsHelper
   def greeting
-    current_time = Time.now.to_i
-    midnight = Time.now.beginning_of_day.to_i
-    noon = Time.now.middle_of_day.to_i
-    five_pm = Time.now.change(:hour => 17 ).to_i
-    eight_pm = Time.now.change(:hour => 20 ).to_i
+    current_time = Time.zone.now.to_i
+    midnight = Time.zone.now.beginning_of_day.to_i
+    noon = Time.zone.now.middle_of_day.to_i
+    five_pm = Time.zone.now.change(:hour => 17 ).to_i
+    eight_pm = Time.zone.now.change(:hour => 20 ).to_i
 
     case
     when midnight.upto(noon).include?(current_time)
       "Good morning"
     when noon.upto(five_pm).include?(current_time)
-       "Good afternoon"
+      "Good afternoon"
     when five_pm.upto(eight_pm).include?(current_time)
       "Good evening"
     when eight_pm.upto(midnight + 1.day).include?(current_time)
